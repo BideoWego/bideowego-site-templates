cc3352c7cbed411636b6e7aaa92652490e352f9b
Bideo Wego <bideowego@gmail.com>
2016-01-20 22:15:21 -0500

    Add blog archive javascript

--- assets/javascripts/folder.js
+++ assets/javascripts/folder.js
@@ -1,8 +1,3 @@
-// =========
-//JavaScript
-// =========
-
-
 // ----------------------------------------
 // Folder
 // ----------------------------------------
@@ -74,30 +69,4 @@ $.fn.folder = function(options) {
 
   return this;
 };
-
-// ------------------------------------------------
-// Blog Archive
-// ------------------------------------------------
-
-if ($('#blog-archive').length) {
-
-  // Fold up archives to start
-  $('.posts, .month').hide();
-
-  // Toggle icons to start
-  $('.fa-folder-open')
-    .removeClass('fa-folder-open')
-    .addClass('fa-folder');
-  $('.fa-minus')
-    .removeClass('fa-minus')
-    .addClass('fa-plus');
-  
-  // Set folder events
-  $('.folder.month').folder();
-  $('.folder.year').folder({
-    iconOpen: 'fa-folder-open',
-    iconClosed: 'fa-folder'
-  });
-
-}
 
