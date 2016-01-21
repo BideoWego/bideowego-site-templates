e1a99d6882c884b38bdfab537c4e7fad2f45263f
Bideo Wego <bideowego@gmail.com>
2016-01-20 22:15:53 -0500

    Add bash javascript concatenater

---
+++ js-build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+OUT="$PWD/assets/js/script.js"
+
+FILES=(
+  'folder'
+  'blog/archive'
+)
+
+# --------------------------------------------
+
+echo "Files changed!"
+echo "Building JavaScripts..."
+
+touch $OUT
+: > $OUT
+echo "// =========" >> $OUT
+echo "//JavaScript" >> $OUT
+echo "// =========" >> $OUT
+echo >> $OUT
+echo >> $OUT
+
+for FILE in "${FILES[@]}"
+do
+  cat "$PWD/assets/javascripts/${FILE}.js" >> $OUT
+done
+
+
+echo "Done."
+echo
+
